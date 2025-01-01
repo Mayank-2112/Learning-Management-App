b1df4fa70a8de6eadcbf3155a5c3b65caaf6b116
Mayank2112 <30808mayank21@gmail.com>
2025-01-01 18:52:11 +0530

    hero section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Navbar from "@/components/Navbar";
+import Landing from "./(nondashboard)/hero/page";
 
 export default function Home() {
   return (
     <div className="nondashboard-layout">
       <Navbar/>
+      <main className="nondashboard-layout__main">
+        <Landing />
+      </main>
     </div>
   );
 }
